55332e6f7c8b544ccc664410cc3216b657502f5d
GayatriJoshi4395 <joshigayatri177@gmail.com>
2025-11-18 09:22:22 +0530

    Routes Created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/AboutUs'
+import Contact from './components/Contact'
+import NavbarMain from './components/NavbarMain'
 
 function App() {
 
   return (
-    <>
+    <Router>
+      <NavbarMain/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+      </Routes>
       
-    </>
+    </Router>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+const Home = () => {
+  return (
+    <div className='container1'>
+        <div className='home'>
+            <h1>Home Page</h1>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
